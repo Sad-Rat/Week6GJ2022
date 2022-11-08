fe247686581001a3539b48570aee9b8035d2bba8
P2422667 <P2422667@dmu.ac.uk>
2022-11-08 15:55:54 +0000

    score and timer but no score and timer

--- code/src/UI.cpp
+++ code/src/UI.cpp
@@ -3,7 +3,8 @@
 UI::UI(float winWidth, float winHeight, std::shared_ptr<Texture> pWT)
 {
 	hpTexture.reset(new Texture("../assets/textures/fontTest.png"));
-	scoreTexture.reset(new Texture("../assets/textures/scoreTexture.png"));
+	timerTexture.reset(new Texture("../assets/textures/timerTexture.png"));
+	scoreTexture.reset(new Texture("../assets/textures/scoreTexture.png")),
 	m_view = glm::mat4(1.0f);
 	m_proj = glm::ortho(0.f, winWidth, 0.f, winHeight);
 	plainWhiteTexture = pWT;
@@ -24,7 +25,8 @@ void UI::onRender()
 	//Renderer::drawQuad(Quad::createCentreHalfExtents(glm::vec2(512, 400), glm::vec2(400, 400), 0, false), *hpTexture, glm::vec4(1.0f));
 	Renderer::drawQuad(Quad::createTopLeftExtents(glm::vec2(10, 100), glm::vec2(210, 70)), *hpTexture, glm::vec4(1.0f));
 	Renderer::drawQuad(Quad::createTopLeftExtents(glm::vec2(15, 50), glm::vec2(200 * hpRemaining, 15)), *plainWhiteTexture, glm::vec4(1.0f, 0.0f, 0.0f, 1.0f));
-	Renderer::drawQuad(Quad::createTopLeftExtents(glm::vec2(10, getWinHeight() - 10), glm::vec2(336, 70)), *scoreTexture, glm::vec4(1.0f));
+	Renderer::drawQuad(Quad::createTopLeftExtents(glm::vec2(10, getWinHeight() - 10), glm::vec2(336, 70)), *timerTexture, glm::vec4(1.0f));
+	Renderer::drawQuad(Quad::createTopLeftExtents(glm::vec2(getWinWidth() - 346, getWinHeight() - 10), glm::vec2(336, 70)), *scoreTexture, glm::vec4(1.0f));
 	Renderer::disableBlend();
 
 	Renderer::end();
