f965e8fb757eb626db395e61eeb0c21b8a91c632
P2422667 <P2422667@dmu.ac.uk>
2022-11-08 14:33:16 +0000

    health UI (non adjustable bar)

--- code/include/UI.h
+++ code/include/UI.h
@@ -8,9 +8,15 @@ public:
 	UI(float winWidth, float winHeight, std::shared_ptr<Texture> pWT);
 	~UI();
 	void onRender();
+	inline float getWinWidth() { return w_width; }
+	inline float getWinHeight() { return w_height; }
+	inline void setWinWidth(float width) { w_width = width; }
+	inline void setWinHeight(float height) { w_height = height; }
 private:
 	std::shared_ptr<Texture> hpTexture;
 	std::shared_ptr<Texture> plainWhiteTexture;
 	glm::mat4 m_view;
 	glm::mat4 m_proj;
+	float w_width;
+	float w_height;
 };
--- code/src/UI.cpp
+++ code/src/UI.cpp
@@ -2,10 +2,12 @@
 
 UI::UI(float winWidth, float winHeight, std::shared_ptr<Texture> pWT)
 {
-	hpTexture.reset(new Texture("../assets/textures/moon.png"));
+	hpTexture.reset(new Texture("../assets/textures/fontTest.png"));
 	m_view = glm::mat4(1.0f);
 	m_proj = glm::ortho(0.f, winWidth, 0.f, winHeight);
 	plainWhiteTexture = pWT;
+	setWinHeight(winHeight);
+	setWinWidth(winWidth);
 }
 
 UI::~UI()
@@ -17,8 +19,9 @@ void UI::onRender()
 	Renderer::begin(m_view, m_proj);
 
 	Renderer::enableStandardBlend();
-	Renderer::drawQuad(Quad::createCentreHalfExtents(glm::vec2(512, 400), glm::vec2(400, 400), 0, false), *hpTexture, glm::vec4(1.0f));
-	Renderer::drawQuad(Quad::createTopLeftExtents(glm::vec2(512, 400), glm::vec2(200, 100)), *plainWhiteTexture, glm::vec4(1.0f, 0.0f, 0.0f, 1.0f));
+	//Renderer::drawQuad(Quad::createCentreHalfExtents(glm::vec2(512, 400), glm::vec2(400, 400), 0, false), *hpTexture, glm::vec4(1.0f));
+	Renderer::drawQuad(Quad::createTopLeftExtents(glm::vec2(10, 100), glm::vec2(420 * 0.5f, 140 * 0.5f)), *hpTexture, glm::vec4(1.0f));
+	Renderer::drawQuad(Quad::createTopLeftExtents(glm::vec2(15, 50), glm::vec2(400 * 0.5f, 30 * 0.5f)), *plainWhiteTexture, glm::vec4(1.0f, 0.0f, 0.0f, 1.0f));
 	Renderer::disableBlend();
 
 	Renderer::end();
